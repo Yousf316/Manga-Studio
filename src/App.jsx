78ece2928319657e518c4afd216a1db12aeb7ec8
Yousf316 <youseframzii@outlook.com>
2024-06-13 21:22:00 +0300

    adds file

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,33 @@ import Manga from "./Pages/Manga"
 import Home from "./Pages/Home"
 import {RouterProvider,createBrowserRouter,BrowserRouter,Route,Routes} from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/Manga-Studio/",
-    element: <Home />,
-    children: [
+// const router = createBrowserRouter([
+//   {
+//     path: "/Manga-Studio/",
+//     element: <Home />,
+//     children: [
       
-      {
-        path: "/Manga-Studio/manga/:id",
-        element: <Manga/>,
-      },
-    ],
-  },
-]);
+//       {
+//         path: "/Manga-Studio/manga/:id",
+//         element: <Manga/>,
+//       },
+//     ],
+//   },
+// ]);
 
 function App() {
 
   return(
     <>
 
-   <RouterProvider router={router} />
+   {/* <RouterProvider router={router} /> */}
 
-    {/* <BrowserRouter basename='/Manga-Studio/'>
+    <BrowserRouter basename='/Manga-Studio/'>
     <Routes>
       <Route index element={<Home/>}/>
       <Route path="/Manga-Studio/manga/:id" element={<Manga/>}/>
     </Routes>
-    </BrowserRouter> */}
+    </BrowserRouter>
     </>
   )
 
